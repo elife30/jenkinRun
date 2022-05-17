71f1fc8881d0b9ee7928782a174beb06cdd478f1
elife <mert_elife@hotmail.com>
2022-05-17 14:20:31 -0400

    secound send

--- src/com/works/Main.java
+++ src/com/works/Main.java
@@ -1,13 +1,15 @@
 package com.works;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
         for (int i = 0; i <10 ; i++) {
             System.out.println("i love  Myself");
+            System.out.println("wonderful");
 
         }
-	// write your code here
+
     }
 }
