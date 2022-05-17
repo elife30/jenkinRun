fa130986043f8a4e07437c00fa28e74ccc82125d
elife <mert_elife@hotmail.com>
2022-05-17 13:31:55 -0400

    i hope i can run jenkin

---
+++ src/com/works/Main.java
@@ -0,0 +1,13 @@
+package com.works;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("i love  Myself");
+
+        }
+	// write your code here
+    }
+}
