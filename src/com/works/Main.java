63ebc17a4accfdc0a39655231295ac8c474cb8cc
elife <mert_elife@hotmail.com>
2022-05-19 13:41:34 -0400

    may19

--- src/com/works/Main.java
+++ src/com/works/Main.java
@@ -7,9 +7,11 @@ public class Main {
 
         for (int i = 0; i <10 ; i++) {
             System.out.println("i love  Myself");
-            System.out.println("wonderful");
+
 
         }
 
+        System.out.println("wonderful");
+        System.out.println("i feel so sad");
     }
 }
