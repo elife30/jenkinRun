099da31e6fcc8b24725596a365cebc1eac13a26c
elife <mert_elife@hotmail.com>
2022-05-20 14:37:59 -0400

    may20 Java

--- src/com/works/Main.java
+++ src/com/works/Main.java
@@ -13,5 +13,7 @@ public class Main {
 
         System.out.println("wonderful");
         System.out.println("i feel so sad");
+
+
     }
 }
---
+++ src/com/works/java/may20.java
@@ -0,0 +1,24 @@
+package com.works.java;
+
+public class may20 {
+    public static void main(String[] args) {
+        //Java Program to Find Odd or Even number
+
+
+        int number = 5;
+        if (number % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+        //>>>>>>>>>>>
+        int i = 20;
+        System.out.println(i == 22);
+
+
+    }
+
+
+
+
+}
